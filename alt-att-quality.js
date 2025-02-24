afaa5304e8560021de5909e8c4067a1225feedf0
albertomeleleo <albertomeleleo@gmail.com>
2025-02-24 15:31:33 +0100

    code structuring and results table
    code structuring and results table

--- alt-att-quality.js
+++ alt-att-quality.js
@@ -1,4 +1,5 @@
-import { Audit } from "lighthouse";
+import { Audit } from "lighthouse"; 
+import { validateAltText } from "./alt-text-validator.js"; 
 
 class AltAttQualityAudit extends Audit {
   static get meta() {
@@ -12,14 +13,62 @@ class AltAttQualityAudit extends Audit {
     };
   }
 
-  static audit(artifacts) {
-    const imgs = artifacts.ImageElements;
-    //TODO: implement audit logic here
-    console.log(imgs);
-    const score = 0;//imgs ? 1 : 0;
+  static async audit(artifacts,context) {
+    const imgs = artifacts.ImageElements || [];
+    const largeImgs = this.removeNonEvaluableImages(imgs,context);
+    const lowQualityAltImgs = await this.evaluateTagAltContent(largeImgs, context);
+    const score = lowQualityAltImgs.length === 0 ? 1 : 0;
 
+    return this.generateLightHouseTable(score, lowQualityAltImgs);
+  }
+
+  static removeNonEvaluableImages(imgs, context) {
+    const minWidthSize = context.settings.minWidthSize || 100;
+    const minHeightSize = context.settings.minHeightSize || 100;
+    const largeImgs = imgs.filter(img => img.displayedWidth > minWidthSize && img.displayedHeight > minHeightSize
+    );
+    console.log("Filtered images:", largeImgs);
+    return largeImgs;
+  }
+
+  static async evaluateTagAltContent(largeImgs, context) {
+    
+    const lowQualityAltImgs = largeImgs.filter(img => !img.alt || img.alt.trim().length < 5);
+   /* const lowQualityAltImgs = [];
+    for (const img of largeImgs) {
+      try {
+        const validationResult = await validateAltText(img.src, img.alt, context);
+        const scoreFromApi = parseInt(validationResult.candidates[0].content);
+
+        lowQualityAltImgs.push({ ...img, apiScore: scoreFromApi, alt: img.alt || "(empty)" });
+      } catch (error) {
+        console.error("Error validating image:", img.src, error);
+        lowQualityAltImgs.push({ ...img, apiScore: 0, alt: img.alt || "(empty)", error: error.message });
+      }
+    }*/
+    return lowQualityAltImgs;
+  }
+  
+  static generateLightHouseTable(score, lowQualityAltImgs) {
     return {
       score,
+      details: {
+        type: "table",
+        headings: [
+          { key: "thumbnail", itemType: "thumbnail", text: "Preview" },
+          { key: "src", itemType: "url", text: "Image URL" },
+          { key: "alt", itemType: "text", text: "Alt Attribute" },
+          //{ key: "score", itemType: "numeric", text: "Score" },
+          ...(score === 0 ? [{ key: "htmlTag", itemType: "text", text: "HTML Tag" }] : [])
+        ],
+        items: lowQualityAltImgs.map((img) => ({
+          thumbnail: img.src,
+          src: img.src,
+          alt: img.alt || "(empty)",
+          // score: score,
+          ...(score === 0 ? { htmlTag: img.node.snippet } : {})
+        })),
+      },
     };
   }
 }
--- custom-config.cjs
+++ custom-config.cjs
@@ -1,6 +1,13 @@
 module.exports = {
     extends: "lighthouse:default",
     audits: ["alt-att-quality"],
+    minWidthSize: 100,
+    minHeightSize: 100,
+    PROJECT_ID : "seetheunseen",
+    LOCATION_ID : "europe-west8",
+    API_ENDPOINT : "europe-west8-aiplatform.googleapis.com",
+    MODEL_ID : "gemini-2.0-flash-001",
+    GENERATE_CONTENT_API : "streamGenerateContent",
     categories: {
       accessibility: {
         title: "Accessibility",
